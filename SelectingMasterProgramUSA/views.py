1319054c71d7885e82f8bfa5ffcf62ff36edd904
dry950526@gmail.com <dry9505261608>
2017-04-20 15:57:03 +0800

    fix db bug

--- SelectingMasterProgramUSA/views.py
+++ SelectingMasterProgramUSA/views.py
@@ -1,21 +1,22 @@
 from flask import request
-from SelectingMasterProgramUSA import app, all_data
+from SelectingMasterProgramUSA import app, client
 from SelectingMasterProgramUSA.models import Admission
 import jinja2
 import pprint
 from flask import render_template
+from pymongo import MongoClient
 
-@app.route('/', methods=['GET'])
-def main_page():
-    return render_template('main.html')
-
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['POST','GET'])
 def query_university():
-    school_name = request.form['name']
-    major = request.form['major']
-    result = all_data.find({'admission_school': school_name, 'major': major})
-    for ad in result:
-        pprint.pprint(ad)
-
+    if request.method == "POST":
+        #client = MongoClient('localhost', 27017)
+        db = client.settings
+        all_data = db['MONGODB_DBNAME.Admissions']
+        school_name = request.form['name']
+        major = request.form['major']
+        result = all_data.find({'admission_school': school_name, 'major': major})
+        for ad in result:
+            pprint.pprint(ad)
+    return render_template('main.html')
 
 
