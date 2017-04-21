0d3f61a40703ec4e9aee4b03abb0a07c484a3929
JohnDing1995 <dry950526@gmail.com>
2017-04-21 13:58:07 +0800

    display query result on webpage, composing needed

--- SelectingMasterProgramUSA/views.py
+++ SelectingMasterProgramUSA/views.py
@@ -6,18 +6,19 @@ import pprint
 from flask import render_template
 from pymongo import MongoClient
 
-@app.route('/',methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def query_university():
+    query_result = "Please Enter The University Name and Major Which You Want to Apply"
     if request.method == "POST":
-        #client = MongoClient('localhost', 27017)
+        # client = MongoClient('localhost', 27017)
         db = client.settings
         all_data = db['MONGODB_DBNAME.Admissions']
         school_name = request.form['name']
         major = request.form['major']
-        result = all_data.find({'admission_school': school_name, 'major': major})
-        #for ad in result:
-        #    pprint.pprint(ad)
-        #for test only
-    return render_template('main.html')
-
+        query_result = all_data.find({'admission_school': school_name, 'major': major})
+        #for ad in query_result:
+           #pprint.pprint(ad)
+        # for test only
+    return render_template('main.html', query_result =query_result)
 
