4bb10c0767932b0ab40a763cab8b1dbaecacdbb3
JohnDing1995 <dry950526@gmail.com>
2017-04-21 15:04:33 +0800

    update

--- SelectingMasterProgramUSA/views.py
+++ SelectingMasterProgramUSA/views.py
@@ -9,16 +9,18 @@ from pymongo import MongoClient
 
 @app.route('/', methods=['POST', 'GET'])
 def query_university():
-    query_result = "Please Enter The University Name and Major Which You Want to Apply"
+    query_result = ""
     if request.method == "POST":
         # client = MongoClient('localhost', 27017)
         db = client.settings
         all_data = db['MONGODB_DBNAME.Admissions']
-        school_name = request.form['name']
-        major = request.form['major']
-        query_result = all_data.find({'admission_school': school_name, 'major': major})
-        #for ad in query_result:
-           #pprint.pprint(ad)
+        school_name_re = ".*" + request.form['name'] + ".*"
+        major_re = ".*" + request.form['major'] + ".*"
+        query_result = all_data.find({"admission_school":{'$regex':school_name_re,'$options':'$i'},
+                                      "major":{'$regex':major_re,'$options':'$i'}})
+        print(type(query_result))
+        for ad in query_result:
+           pprint.pprint(ad)
         # for test only
     return render_template('main.html', query_result =query_result)
 
